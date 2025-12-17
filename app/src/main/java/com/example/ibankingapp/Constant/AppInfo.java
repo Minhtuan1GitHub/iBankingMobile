b6e1d366befaed222f0ac249a454ac26e29dcc0e
Tam Nguyen <ngctam.3108@gmail.com>
2025-12-17 16:14:42 +0700

    SỬA KEY VNPAY

--- app/src/main/java/com/example/ibankingapp/Constant/AppInfo.java
+++ app/src/main/java/com/example/ibankingapp/Constant/AppInfo.java
@@ -3,7 +3,7 @@ package com.example.ibankingapp.Constant;
 public class AppInfo {
     // Thông tin cấu hình VNPay Sandbox (Môi trường Test)
     public static final String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static final String vnp_TmnCode = "24JRSKQZ"; // Mã Website (Terminal ID) Test
-    public static final String vnp_HashSecret = "NVXWBVUDKGFWTRWJMIGBBAVPSIBJOFFL"; // Secret Key Test
+    public static final String vnp_TmnCode = "XG60YY6A"; // Mã Website (Terminal ID) Test
+    public static final String vnp_HashSecret = "XH22EK552B4G83RN180O0DQF9L0K8OGV"; // Secret Key Test
     public static final String vnp_ReturnUrl = "ibanking://result"; // Phải khớp với Scheme trong AndroidManifest
 }
